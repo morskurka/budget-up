3310eb8aa50be6aa98cab220b9d80f8cab4d95a0
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2021-11-25 18:25:33 +0200

    add basic components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import Category from "./components/Category";
+import CategoryList from "./components/CategoryList";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="App">Welcome to BudgetUP!</div>
+      <div className="container">
+        <div className="App">Welcome to BudgetUP!</div>
+        <CategoryList />
+      </div>
+
+      <Footer />
     </>
   );
 }
---
+++ src/components/Category.jsx
@@ -0,0 +1,14 @@
+import ProgressBarBU from "./ProgressBarBU";
+
+const Category = () => {
+  return (
+    <div className="card">
+      <div className="card-body">
+        Category
+        <ProgressBarBU />
+      </div>
+    </div>
+  );
+};
+
+export default Category;
