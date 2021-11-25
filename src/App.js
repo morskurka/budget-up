71971aa5f4215747a54b12ddd9947067ceadbb35
morskurka <57572844+morskurka@users.noreply.github.com>
2021-11-25 17:36:15 +0200

    add basic navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App">Welcome to BudgetUP!</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="App">Welcome to BudgetUP!</div>
+    </>
+  );
 }
 
 export default App;
