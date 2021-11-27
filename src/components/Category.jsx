baad4134927cddfc8deb9ece9b9911a999c401b0
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2021-11-27 13:37:45 +0200

    implement basic categories components

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,11 +1,32 @@
 import ProgressBarBU from "./ProgressBarBU";
 
-const Category = () => {
+const Category = (props) => {
   return (
     <div className="card">
       <div className="card-body">
-        Category
-        <ProgressBarBU />
+        <div className="row justify-content-around align-items-center">
+          <div className="col-4">
+            <i class={"bi fs-4 bi-" + props.icon}></i>
+            <div className="card-title fs-4">{props.name}</div>
+          </div>
+          <div className="col-6">
+            <div className="row justify-content-between">
+              <div className="col">
+                <div className="lead">Current</div>
+                <div className="fw-bold">{props.currBalance + "$"}</div>
+              </div>
+              <div className="col text-end">
+                <div className="lead">Expected</div>
+                <div className="fw-bold">{props.totalExpected + "$"}</div>
+              </div>
+            </div>
+
+            <ProgressBarBU
+              currBalance={props.currBalance}
+              totalExpected={props.totalExpected}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,15 +1,44 @@
 import Category from "./Category";
 
 const CategoryList = () => {
+  const Supermarket = {
+    icon: "shop",
+    name: "Supermarket",
+    currBalance: 1200,
+    totalExpected: 1450,
+  };
+
+  const Electricity = {
+    icon: "plug",
+    name: "Electricity",
+    currBalance: 700,
+    totalExpected: 1200,
+  };
+
+  const Water = {
+    icon: "water",
+    name: "Water",
+    currBalance: 120,
+    totalExpected: 20,
+  };
+
+  const categories = [Supermarket, Electricity, Water];
+
   return (
-    <ul className="card">
-      <li className="m-2">
-        <Category />
-      </li>
-      <li className="m-2">
-        <Category />
-      </li>
-    </ul>
+    <div className="list-group">
+      {categories.map((category) => {
+        return (
+          <div className="m-2">
+            <Category
+              name={category.name}
+              icon={category.icon}
+              currBalance={category.currBalance}
+              totalExpected={category.totalExpected}
+            />
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
