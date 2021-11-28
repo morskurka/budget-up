50376fcbc3f3258514dcb5298b7bc93ec5a984f4
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2021-11-28 14:40:21 +0200

    small fixes to Categories components layout

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,30 +1,38 @@
 import ProgressBarBU from "./ProgressBarBU";
 
 const Category = (props) => {
+  const percentages = (props.currBalance / props.totalExpected) * 100;
+
   return (
-    <div className="card rounded-pill">
+    <div className="card rounded-pill bg-light">
       <div className="card-body">
         <div className="row justify-content-around align-items-center">
           <div className="col-4">
-            <i class={"bi fs-4 bi-" + props.icon}></i>
-            <div className="card-title fs-4">{props.name}</div>
+            <div className="card-title fs-4">
+              <i class={"bi fs-4 bi-" + props.icon}></i>
+              {" " + props.name}
+            </div>
           </div>
           <div className="col-6">
             <div className="row justify-content-between">
               <div className="col">
-                <div className="lead">Current</div>
-                <div className="fw-bold">{props.currBalance + "$"}</div>
+                <div className="fw-light">Current</div>
+                <div
+                  className={
+                    (percentages > 100 ? "text-danger " : "text-success ") +
+                    "fw-bold lead"
+                  }
+                >
+                  {props.currBalance + "$"}
+                </div>
               </div>
               <div className="col text-end">
-                <div className="lead">Expected</div>
-                <div className="fw-bold">{props.totalExpected + "$"}</div>
+                <div className="fw-light">Expected</div>
+                <div className="fw-bold lead">{props.totalExpected + "$"}</div>
               </div>
             </div>
 
-            <ProgressBarBU
-              currBalance={props.currBalance}
-              totalExpected={props.totalExpected}
-            />
+            <ProgressBarBU percentages={percentages} />
           </div>
         </div>
       </div>
