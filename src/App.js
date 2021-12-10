083d7037c1960ebc6039762a5a580dcd93a1c95d
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2021-12-10 13:40:12 +0200

    Add basic routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import CategoryPage from "./pages/CategoryPage";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div>
-        <div className="App"></div>
-        <CategoryPage />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/CategoryPage" element={<CategoryPage />} />
+        </Routes>
+      </Router>
 
       <Footer />
     </>
