f6501d1ffdc7da8d2b97bcb3237c3db6fbbb6e6d
morskurka <57572844+morskurka@users.noreply.github.com>
2021-12-10 12:59:44 +0200

    change data table

--- src/components/CategoryTable.jsx
+++ src/components/CategoryTable.jsx
@@ -5,29 +5,30 @@ const CategoryTable = () => {
         <thead class="table-light">
           <tr>
             <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Date</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Business</th>
           </tr>
         </thead>
         <tbody>
           {" "}
           <tr>
             <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td>13/1</td>
+            <td>1200</td>
+            <td>Shufersal</td>
           </tr>
           <tr>
             <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
+            <td>16/1</td>
+            <td>100</td>
+            <td>Shufersal</td>
           </tr>
           <tr>
             <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
+            <td>21/1</td>
+            <td>200</td>
+            <td>Shufersal</td>
           </tr>
         </tbody>
       </table>
