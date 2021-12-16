0a55a432bb9e2d0019b76e31feb177d75c49535f
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2021-12-16 21:33:19 +0200

    impl basic logic (add, remove, update reducer)

--- src/components/CashExpense.jsx
+++ src/components/CashExpense.jsx
@@ -1,12 +1,32 @@
-const CashExpense = (props) => {
+import { useContext, useRef, useState } from "react";
+import { GlobalContext } from "../contexts/GlobalState";
+
+const CashExpense = ({ id, updateExpensesList, handleDeleteBtnClicked }) => {
+  const [category, setCategory] = useState("");
+  const amount = useRef();
+  const { addTransaction } = useContext(GlobalContext);
+
+  function handleCategoryChange(e) {
+    setCategory(e.target.value);
+    updateExpensesList(
+      parseInt(id),
+      e.target.value,
+      parseInt(amount.current.value)
+    );
+  }
+
   return (
     <>
       <div
         className="row align-items-center m-4 justify-content-center"
-        id={"cashExpenseRow" + props.id}
+        id={"cashExpenseRow" + id}
       >
         <div className="col-sm-6 col-md-4 mb-2">
-          <select className="form-select form-select-md">
+          <select
+            className="form-select form-select-md"
+            name="category"
+            onChange={handleCategoryChange}
+          >
             <option value="">Select Category</option>
             <option value="Supermarket">Supermarket</option>
             <option value="Water">Water</option>
@@ -15,8 +35,8 @@ const CashExpense = (props) => {
           </select>
         </div>
         <div className="col-sm-4 col-md-3 col-lg-2 mb-2">
-          <div class="input-group">
-            <span class="input-group-text" id="basic-addon1">
+          <div className="input-group">
+            <span className="input-group-text" id="basic-addon1">
               $
             </span>
             <input
@@ -24,12 +44,23 @@ const CashExpense = (props) => {
               min="0"
               className="form-control"
               id="cashAmount"
+              ref={amount}
+              onChange={() =>
+                updateExpensesList(
+                  parseInt(id),
+                  category,
+                  parseInt(amount.current.value)
+                )
+              }
             />
           </div>
         </div>
         <div className="col-sm-2 mb-2">
-          <button className="btn btn-danger">
-            <i class="bi bi-eraser"></i>
+          <button
+            className="btn btn-danger"
+            onClick={() => handleDeleteBtnClicked(id)}
+          >
+            <i className="bi bi-eraser"></i>
           </button>
         </div>
       </div>
--- src/pages/CashExpensesPage.jsx
+++ src/pages/CashExpensesPage.jsx
@@ -1,7 +1,79 @@
+import { useEffect, useState, useContext } from "react";
 import BalanceInfoBar from "../components/BalanceInfoBar";
 import CashExpense from "../components/CashExpense";
+import { GlobalContext } from "../contexts/GlobalState";
 
 const CashExpensesPage = () => {
+  // add / remove CashExpense Components
+  const [ExpensesComponents, setExpensesComponents] = useState([
+    <CashExpense
+      id={0}
+      key={0}
+      updateExpensesList={addExpenseObject}
+      handleDeleteBtnClicked={removeExpenseItem}
+    />,
+  ]);
+
+  function addExpenseItem() {
+    let newId = ExpensesComponents[ExpensesComponents.length - 1].props.id + 1;
+    setExpensesComponents([
+      ...ExpensesComponents,
+      <CashExpense
+        id={newId}
+        key={newId}
+        updateExpensesList={addExpenseObject}
+        handleDeleteBtnClicked={removeExpenseItem}
+      />,
+    ]);
+  }
+
+  function removeExpenseItem(id) {
+    if (id !== 0) {
+      // remove from DOM
+      let newExpensesComponents = ExpensesComponents.filter(
+        (item) => item.key !== id
+      );
+      // remove from ExpensesList
+      setExpensesComponents(newExpensesComponents);
+      let newExpensesList = ExpensesList.filter((item) => item.id !== id);
+      setExpensesList(newExpensesList);
+    }
+  }
+
+  // store expanses details
+  const [ExpensesList, setExpensesList] = useState([]);
+  function addExpenseObject(id, category, amount) {
+    let newExpensesList = [...ExpensesList];
+    newExpensesList[id] = { id, category, amount };
+    setExpensesList(newExpensesList);
+  }
+
+  // handle unclassified sum
+  const [sum, setSum] = useState(0);
+
+  function updateSum() {
+    let newSum = ExpensesList.reduce((prevVal, nextVal) => {
+      prevVal = isNaN(prevVal) ? 0 : prevVal;
+      nextVal.amount = isNaN(nextVal.amount) ? 0 : nextVal.amount;
+      return prevVal + nextVal.amount;
+    }, 0);
+    setSum(newSum);
+  }
+
+  // update the sum each time the list is changed
+  useEffect(() => {
+    updateSum();
+  }, [ExpensesList]);
+
+  const { addTransaction } = useContext(GlobalContext);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (ExpensesList.length > 0) {
+      // TODO: add transactions to database
+      ExpensesList.forEach((expense) => addTransaction(expense));
+    }
+  };
+
   return (
     <>
       <BalanceInfoBar backgroundColor="bg-light" barColor="bg-success" />
@@ -12,7 +84,7 @@ const CashExpensesPage = () => {
             <div className="card m-2">
               <div className="card-body p-4 bg-light">
                 <h1 className="card-title ms-3 mb-4 display-6 align-items-center">
-                  <i class="bi bi-cash-coin me-4"></i>
+                  <i className="bi bi-cash-coin me-4"></i>
                   Classify Your Cash Withdrawals
                 </h1>
                 <div className="row justify-content-center">
@@ -22,20 +94,30 @@ const CashExpensesPage = () => {
                         <h3 className="fw-light">
                           Unclassified Cash Withdrawal
                         </h3>
-                        <h2>1500$</h2>
+                        {/* TODO: implement logic instead of CONST value */}
+                        <h2>{1500 - sum}$</h2>
                       </div>
                     </div>
                   </div>
                 </div>
                 <div>
-                  <CashExpense id="1" />
-                  <CashExpense id="2" />
-                  <CashExpense id="3" />
-                  <CashExpense id="4" />
+                  <div>{ExpensesComponents}</div>
+                  <div className="ms-5">
+                    {/* ADD EXPENSE COMPONENT */}
+                    <button
+                      className="btn btn-success"
+                      onClick={addExpenseItem}
+                    >
+                      <i className="bi bi-plus-square"></i>
+                    </button>
+                  </div>
                 </div>
-                <div className="ms-5">
-                  <button className="btn btn-success">
-                    <i class="bi bi-plus-square"></i>
+                <div className="align-items-center justify-content-center d-flex">
+                  <button
+                    className="btn btn-success btn-lg rounded-pill"
+                    onClick={onSubmit}
+                  >
+                    Save
                   </button>
                 </div>
               </div>
