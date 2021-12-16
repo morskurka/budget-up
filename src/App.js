7fdd03c185b8c4227a8d93c13859db4220241939
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2021-12-16 18:28:27 +0200

    add basic Hooks

--- src/App.js
+++ src/App.js
@@ -8,21 +8,24 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CashExpensesPage from "./pages/CashExpensesPage";
 import CashIncomesPage from "./pages/CashIncomesPage";
+import { GlobalProvider } from "./contexts/GlobalState";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/CategoryRow" element={<CategoryRow />} />
-          <Route path="/CategoryPage" element={<CategoryPage />} />
-          <Route path="/CashExpensesPage" element={<CashExpensesPage />} />
-          <Route path="/CashIncomesPage" element={<CashIncomesPage />} />
-        </Routes>
-      </Router>
-      <Footer />
+      <GlobalProvider>
+        <Navbar />
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/CategoryRow" element={<CategoryRow />} />
+            <Route path="/CategoryPage" element={<CategoryPage />} />
+            <Route path="/CashExpensesPage" element={<CashExpensesPage />} />
+            <Route path="/CashIncomesPage" element={<CashIncomesPage />} />
+          </Routes>
+        </Router>
+        <Footer />
+      </GlobalProvider>
     </>
   );
 }
