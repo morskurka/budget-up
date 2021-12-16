890a503534294dce50f6845a9ab5351de10cfaab
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2021-12-16 13:13:17 +0200

    remove second version of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,138 +1,238 @@
 const Footer = () => {
   return (
-    <div>
-      <footer className="text-dark" style={{ backgroundColor: "#ECEFF1" }}>
-        <section
-          className="d-flex justify-content-between p-4 text-white"
-          style={{ backgroundColor: "#21D192" }}
-        >
-          <div className="me-5">
-            <span>Get connected with us on social networks:</span>
-          </div>
-
-          <div>
-            <a href="" className="text-white me-4">
-              <i className="bi bi-facebook"></i>
-            </a>
-            <a href="" className="text-white me-4">
-              <i className="bi bi-instagram"></i>
-            </a>
-            <a href="" className="text-white me-4">
-              <i className="bi bi-linkedin"></i>
-            </a>
-            <a href="" className="text-white me-4">
-              <i className="bi bi-github"></i>
-            </a>
-          </div>
-        </section>
-
-        <section>
-          <div className="container mt-5">
-            <div className="row mt-3">
-              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                <a className="navbar-brand text-dark fw-bold fs-2" href="#">
-                  <img
-                    className="mx-2"
-                    src="logo192.png"
-                    alt=""
-                    width="30"
-                    height="24"
-                  />
-                  BudgetUP
+    <footer
+      className="position-relative"
+      style={{ background: "#ECEFF1", paddingTop: "30px" }}
+    >
+      <div className="container" style={{ padding: "0 30px" }}>
+        <div className="row">
+          <div className="col-md-4 col-lg-4 col-xl-3 mx-auto mb-4">
+            <div style={{ marginBottom: "20px" }}>
+              <a className="navbar-brand fw-bold fs-3" href="#0">
+                <img
+                  className="pe-1"
+                  style={{ maxWidth: "70px" }}
+                  src="logo192.png"
+                  alt=""
+                />
+                BudgetUp
+              </a>
+            </div>
+            <p className="pe-4 mb-5">
+              Bootstrap 5 UI Kit for Startup Landing Page to Build Rapid
+              User-interface for your next web projects.
+            </p>
+            <ul className="d-flex justify-content-start p-0 mb-1 list-unstyled">
+              <li>
+                <a
+                  className="d-flex justify-content-center align-items-center me-2"
+                  href="#0"
+                  style={{
+                    borderRadius: "50%",
+                    color: "#ffffff",
+                    width: "44px",
+                    height: "44px",
+                    background: "rgba(47, 128, 237, 0.4)",
+                    fontSize: "20px",
+                  }}
+                >
+                  <i className="bi bi-facebook"></i>
                 </a>
-                <p className="pt-3">
-                  some text some text some text some text some text some text
-                  some text some text some text some text
-                </p>
-              </div>
-
-              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold">services</h6>
-                <hr
-                  className="mb-4 mt-0 d-inline-block mx-auto"
+              </li>
+              <li>
+                <a
+                  className="d-flex justify-content-center align-items-center me-2"
+                  href="#0"
                   style={{
-                    width: "60px",
-                    backgroundColor: "#7c4dff",
-                    height: "2px",
+                    borderRadius: "50%",
+                    color: "#ffffff",
+                    width: "44px",
+                    height: "44px",
+                    background: "rgba(47, 128, 237, 0.4)",
+                    fontSize: "20px",
                   }}
-                />
-                <p>
-                  <a href="#" className="text-dark">
-                    About Us
-                  </a>
-                </p>
-                <p>
-                  <a href="#" className="text-dark">
-                    Services
-                  </a>
-                </p>
-              </div>
-
-              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold">Useful links</h6>
-                <hr
-                  className="mb-4 mt-0 d-inline-block mx-auto"
+                >
+                  <i className="bi bi-github"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  className="d-flex justify-content-center align-items-center me-2"
+                  href="#0"
                   style={{
-                    width: "60px",
-                    backgroundColor: "#7c4dff",
-                    height: "2px",
+                    borderRadius: "50%",
+                    color: "#ffffff",
+                    width: "44px",
+                    height: "44px",
+                    background: "rgba(47, 128, 237, 0.4)",
+                    fontSize: "20px",
                   }}
-                />
-                <p>
-                  <a href="#" className="text-dark">
-                    Home
-                  </a>
-                </p>
-                <p>
-                  <a href="#" className="text-dark">
-                    My Profile
-                  </a>
-                </p>
-                <p>
-                  <a href="#" className="text-dark">
-                    Settings
-                  </a>
-                </p>
-              </div>
-
-              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                <h6 className="text-uppercase fw-bold">Contact</h6>
-                <hr
-                  className="mb-4 mt-0 d-inline-block mx-auto"
+                >
+                  <i className="bi bi-instagram"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  className="d-flex justify-content-center align-items-center"
+                  href="#0"
                   style={{
-                    width: "60px",
-                    backgroundColor: "#7c4dff",
-                    height: "2px",
+                    borderRadius: "50%",
+                    color: "#ffffff",
+                    width: "44px",
+                    height: "44px",
+                    background: "rgba(47, 128, 237, 0.4)",
+                    margin: "0",
+                    fontSize: "20px",
                   }}
-                />
-                <p>
-                  <i className="bi bi-house-fill mr-3"></i> New York, NY 10012,
-                  US
-                </p>
-                <p>
-                  <i className="bi bi-envelope-fill mr-3"></i> info@example.com
-                </p>
-                <p>
-                  <i className="bi bi-telephone-fill mr-3"></i> + 01 234 567 88
-                </p>
-                <p>
-                  <i className="bi bi-telephone-fil mr-3"></i> + 01 234 567 89
-                </p>
-              </div>
+                >
+                  <i className="bi bi-linkedin"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <div>
+              <h6 className="fw-bold text-uppercase">User Info</h6>
+              <hr
+                className="mb-4 mt-0 d-inline-block mx-auto"
+                style={{
+                  width: "60px",
+                  backgroundColor: "#7c4dff",
+                  height: "2px",
+                }}
+              />
+              <p>
+                <a
+                  className="text-decoration-none"
+                  href=""
+                  style={{
+                    color: "#585978",
+                  }}
+                >
+                  My profile
+                </a>
+              </p>
+              <p>
+                <a
+                  className="text-decoration-none"
+                  href=""
+                  style={{
+                    color: "#585978",
+                  }}
+                >
+                  Settings
+                </a>
+              </p>
             </div>
           </div>
-        </section>
-        <div
-          className="text-center p-3"
-          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-        >
-          © 2022 Copyright:
-          <a className="text-dark p-1" href="#">
-            BudgetUp.com
-          </a>
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <div>
+              <h6 className="fw-bold text-uppercase">Links</h6>
+              <hr
+                className="mb-4 mt-0 d-inline-block mx-auto"
+                style={{
+                  width: "60px",
+                  backgroundColor: "#7c4dff",
+                  height: "2px",
+                }}
+              />
+              <p>
+                <a
+                  className="text-decoration-none"
+                  href=""
+                  style={{
+                    color: "#585978",
+                  }}
+                >
+                  Home
+                </a>
+              </p>
+              <p>
+                <a
+                  className="text-decoration-none"
+                  href=""
+                  style={{
+                    color: "#585978",
+                  }}
+                >
+                  About us
+                </a>
+              </p>
+              <p>
+                <a
+                  className="text-decoration-none"
+                  href=""
+                  style={{
+                    color: "#585978",
+                  }}
+                >
+                  Services
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <h6 className="text-uppercase fw-bold">Contact</h6>
+            <hr
+              className="mb-4 mt-0 d-inline-block mx-auto"
+              style={{
+                width: "60px",
+                backgroundColor: "#7c4dff",
+                height: "2px",
+              }}
+            />
+            <p>
+              <i className="bi bi-house-fill mr-3"></i> New York, NY 10012, US
+            </p>
+            <p>
+              <i className="bi bi-envelope-fill mr-3"></i> info@example.com
+            </p>
+            <p>
+              <i className="bi bi-telephone-fill mr-3"></i> + 01 234 567 88
+            </p>
+            <p>
+              <i className="bi bi-telephone-fill mr-3"></i> + 01 234 567 89
+            </p>
+          </div>
         </div>
-      </footer>
-    </div>
+        <div
+          className="pt-3 pb-1"
+          style={{
+            borderTop: "1px solid rgba(88, 89, 120, 0.4)",
+          }}
+        >
+          <p
+            className="text-center"
+            style={{
+              fontSize: "14px",
+              color: "rgba(88, 89, 120, 0.6)",
+            }}
+          >
+            © 2022 Copyright:
+            <a className="ps-1" href="" style={{ color: "inherit" }}>
+              BudgetUp
+            </a>
+          </p>
+        </div>
+        <a
+          className="d-flex justify-content-center align-items-center text-align-center position-absolute"
+          style={{
+            width: "45px",
+            height: "45px",
+            background: "#2F80ED",
+            fontSize: "20px",
+            color: "#ffffff",
+            borderRadius: "5px",
+            cursor: "pointer",
+            bottom: "30px",
+            right: "30px",
+          }}
+          href="#"
+        >
+          <i className="bi bi-caret-up"></i>
+        </a>
+      </div>
+    </footer>
   );
 };
 
