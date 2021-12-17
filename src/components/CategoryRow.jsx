88d9bebb64a215e43be17e941c9beb6f060f92ac
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2021-12-17 23:22:16 +0200

    add BalanceInfoCard

--- src/components/CategoryRow.jsx
+++ src/components/CategoryRow.jsx
@@ -1,6 +1,7 @@
 import CategoryCard from "./CategoryCard";
 import { useState, useContext } from "react";
 import { GlobalContext } from "../contexts/GlobalState";
+import BalanceInfoBar from "../components/BalanceInfoBar";
 
 const CategoryRow = () => {
   const {
@@ -13,51 +14,55 @@ const CategoryRow = () => {
     getCurrentMonthTransactionByCategory(transactions);
 
   return (
-    <section className="bg-white">
-      <div className="container" style={{ padding: "0 30px" }}>
-        <div className={"row"}>
-          {currMonthTransByCategory.map((cat, index) => {
-            if (index % 3 === 0)
-              return (
-                <div className="col-lg-4 col-md-6" key={index}>
-                  <CategoryCard
-                    title={cat.name}
-                    currBalance={cat.sum}
-                    totalExpected={1000}
-                    icon={categoriesIcons[cat.name]}
-                  />
-                </div>
-              );
-          })}
-          {currMonthTransByCategory.map((cat, index) => {
-            if (index % 3 === 1)
-              return (
-                <div className="col-lg-4 col-md-6" key={index}>
-                  <CategoryCard
-                    title={cat.name}
-                    currBalance={cat.sum}
-                    totalExpected={1000}
-                    icon={categoriesIcons[cat.name]}
-                  />
-                </div>
-              );
-          })}
-          {currMonthTransByCategory.map((cat, index) => {
-            if (index % 3 === 2)
-              return (
-                <div className="col-lg-4 col-md-6" key={index}>
-                  <CategoryCard
-                    title={cat.name}
-                    currBalance={cat.sum}
-                    totalExpected={1000}
-                    icon={categoriesIcons[cat.name]}
-                  />
-                </div>
-              );
-          })}
+    <>
+      {" "}
+      <BalanceInfoBar backgroundColor="bg-light" barColor="bg-success" />;
+      <section className="bg-white">
+        <div className="container" style={{ padding: "0 30px" }}>
+          <div className={"row"}>
+            {currMonthTransByCategory.map((cat, index) => {
+              if (index % 3 === 0)
+                return (
+                  <div className="col-lg-4 col-md-6" key={index}>
+                    <CategoryCard
+                      title={cat.name}
+                      currBalance={cat.sum}
+                      totalExpected={1000}
+                      icon={categoriesIcons[cat.name]}
+                    />
+                  </div>
+                );
+            })}
+            {currMonthTransByCategory.map((cat, index) => {
+              if (index % 3 === 1)
+                return (
+                  <div className="col-lg-4 col-md-6" key={index}>
+                    <CategoryCard
+                      title={cat.name}
+                      currBalance={cat.sum}
+                      totalExpected={1000}
+                      icon={categoriesIcons[cat.name]}
+                    />
+                  </div>
+                );
+            })}
+            {currMonthTransByCategory.map((cat, index) => {
+              if (index % 3 === 2)
+                return (
+                  <div className="col-lg-4 col-md-6" key={index}>
+                    <CategoryCard
+                      title={cat.name}
+                      currBalance={cat.sum}
+                      totalExpected={1000}
+                      icon={categoriesIcons[cat.name]}
+                    />
+                  </div>
+                );
+            })}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
