4d8ee1b321d28e6346a534436da87a997cd8f72e
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2021-12-26 21:00:28 +0200

    add scripts for deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 // import express - server lib
 const express = require("express");
+const path = require("path");
+
 // import cors to set CORS options to '*' easily
 const cors = require("cors");
 const dbOperations = require("./dbOperations");
@@ -7,6 +9,9 @@ const dbOperations = require("./dbOperations");
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../build")));
 
 // parse all requests as json
 app.use(express.json());
@@ -20,6 +25,11 @@ app.options("*", cors());
 
 app.get("/api/transactions", async (req, res) => {
   res.json(await dbOperations.getAllTransactionsByUserID(1));
+});
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 });
 
 app.listen(port, () => {
