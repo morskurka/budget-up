45091d34275e3055e6a1a7fc991957d69bd1b910
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2022-01-01 21:57:56 +0200

    add search category

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import CategoryCard from "../components/CategoryCard";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { GlobalContext } from "../contexts/GlobalState";
 import BalanceInfoBar from "../components/BalanceInfoBar";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = ({ setCurrCategory }) => {
   const { categoriesInfo, categoriesIcons, user } = useContext(GlobalContext);
+  const [categories, setCategories] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
   const navigate = useNavigate();
   useEffect(() => {
     if (!user.email) {
@@ -13,14 +16,37 @@ const HomePage = ({ setCurrCategory }) => {
     }
   });
 
+  useEffect(() => {
+    let filteredCategories = categoriesInfo.filter((cat) => {
+      return (
+        cat.category
+          .toLocaleLowerCase()
+          .indexOf(searchInput.toLocaleLowerCase()) !== -1
+      );
+    });
+    setCategories(filteredCategories);
+  }, [searchInput, categoriesInfo]);
+
   return (
     <>
       <BalanceInfoBar backgroundColor="bg-light" barColor="bg-success" />
       {/* <Prediction /> */}
       <section className="bg-white">
         <div className="container" style={{ padding: "0 30px" }}>
+          <div className=" d-flex justify-content-center">
+            <div className="mb-3 form-group has-feedback">
+              <input
+                placeholder="Search Category..."
+                type="text"
+                className="form-control form-control-lg"
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+              ></input>
+            </div>
+          </div>
+
           <div className={"row"}>
-            {categoriesInfo.map((cat, index) => {
+            {categories.map((cat, index) => {
               if (index % 3 === 0)
                 return (
                   <div className="col-lg-4 col-md-6" key={index}>
@@ -36,7 +62,7 @@ const HomePage = ({ setCurrCategory }) => {
                   </div>
                 );
             })}
-            {categoriesInfo.map((cat, index) => {
+            {categories.map((cat, index) => {
               if (index % 3 === 1)
                 return (
                   <div className="col-lg-4 col-md-6" key={index}>
@@ -52,7 +78,7 @@ const HomePage = ({ setCurrCategory }) => {
                   </div>
                 );
             })}
-            {categoriesInfo.map((cat, index) => {
+            {categories.map((cat, index) => {
               if (index % 3 === 2)
                 return (
                   <div className="col-lg-4 col-md-6" key={index}>
