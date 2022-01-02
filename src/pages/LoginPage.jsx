fc18e02db05b12c73ea2e0cdf4c34ddbe5a21c1f
morskurka <57572844+morskurka@users.noreply.github.com>
2022-01-02 20:55:46 +0200

    add - disabled button

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,17 +7,27 @@ const LoginPage = () => {
   const navigate = useNavigate();
   const [type, setType] = useState("");
   //user login details
-  const userEmail = useRef();
-  const userPassword = useRef();
+  const userEmail = useRef("");
+  const userPassword = useRef("");
   const [loginError, setLoginError] = useState("");
   //user registration details
-  const regFirstName = useRef();
-  const regLastName = useRef();
-  const regEmail = useRef();
-  const regPassword = useRef();
+  const regFirstName = useRef("");
+  const regLastName = useRef("");
+  const regEmail = useRef("");
+  const regPassword = useRef("");
   const [regError, setRegError] = useState("");
 
   async function signUp() {
+    if (
+      regFirstName.current.value === "" ||
+      regLastName.current.value === "" ||
+      regEmail.current.value === "" ||
+      regPassword.current.value === ""
+    ) {
+      setRegError("Required fields");
+      return;
+    }
+
     const user = await fetch("/api/registration", {
       method: "POST",
       headers: {
@@ -30,15 +40,23 @@ const LoginPage = () => {
         password: regPassword.current.value,
       }),
     }).then((res) => res.json());
+    console.log(user);
 
-    if (user.rowsAffected[0] == 0) {
-      setRegError("User already exists with the same email address"); // *******************
+    if (user.rowsAffected === 0) {
+      console.log("1");
+      setRegError("User already exists with the same email address");
     } else {
+      console.log("2");
       setType("");
     }
   }
 
   async function login() {
+    if (userEmail.current.value === "" || userPassword.current.value === "") {
+      setLoginError("Email and password are required");
+      return;
+    }
+
     const user = await fetch("/api/login", {
       method: "POST",
       headers: {
@@ -63,17 +81,13 @@ const LoginPage = () => {
       <div className={"login-container" + type}>
         <div className="forms-container">
           <div className="signin-signup">
+            {/*Sign in form*/}
             <form action="#" className="form-section sign-in-form">
               <div className=""></div>
               <h2 className="title">Sign in</h2>
               <div className="input-field">
                 <i className="bi bi-person"></i>
-                <input
-                  type="text"
-                  placeholder="Email"
-                  ref={userEmail}
-                  required
-                />
+                <input type="text" placeholder="Email" ref={userEmail} />
               </div>
               <div className="input-field">
                 <i className="bi bi-lock"></i>
@@ -81,7 +95,6 @@ const LoginPage = () => {
                   type="password"
                   placeholder="Password"
                   ref={userPassword}
-                  required
                 />
               </div>
               <button
@@ -95,6 +108,7 @@ const LoginPage = () => {
               </button>
               <p className="social-text">{loginError}</p>
             </form>
+            {/*Sign up form*/}
             <form action="#" className="form-section sign-up-form">
               <h2 className="title">Sign up</h2>
               <div className="input-field">
