5b92a7722ba4fb1dc4af7b11606c4f31d57917a2
morskurka <57572844+morskurka@users.noreply.github.com>
2022-01-07 01:53:36 +0200

    cash income CSS

--- src/pages/CashIncomesPage.jsx
+++ src/pages/CashIncomesPage.jsx
@@ -3,6 +3,7 @@ import CashIncome from "../components/CashIncome";
 import { useNavigate } from "react-router-dom";
 import { useState, useContext, useEffect } from "react";
 import { GlobalContext } from "../contexts/GlobalState";
+import Header from "../components/Header";
 
 const CashIncomesPage = () => {
   const navigate = useNavigate();
@@ -38,41 +39,60 @@ const CashIncomesPage = () => {
   return (
     <>
       <BalanceInfoBar />
-
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-xl-10">
-            <div className="card m-2">
-              <div className="card-body p-4 bg-light">
-                <h1 className="card-title ms-3 mb-4 display-6 align-items-center">
-                  <i className="bi bi-cash-coin me-4"></i>
-                  Add Your Cash Incomes
-                </h1>
-                <form onSubmit={onSubmit}>
-                  <CashIncome
-                    setSource={setSource}
-                    setDate={setDate}
-                    setAmount={setAmount}
-                  />
-                  <div className="align-items-center justify-content-center d-flex">
-                    <button
-                      className="btn btn-success btn-lg rounded-pill"
-                      /* TODO: implement logic instead of CONST value */
-                      type="submit"
-                      disabled={
-                        amount <= 0 ||
-                        date.trim() === "" ||
-                        source.trim() === ""
-                      }
-                      id="saveBtn"
-                    >
-                      Save
-                    </button>
+      <div className="cash-expenses">
+        <div className="py-5">
+          <Header
+            title="Add Cash Incomes" //**********
+            body="Follow your real financial status by adding your cash incomes and keep tracing them easily"
+          />
+        </div>
+        <div className="container">
+          <section className="cash-expense-section">
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-lg-10">
+                  <div className="single-cash-expense">
+                    <div className="info">
+                      <h5>
+                        <i className="bi bi-cash-coin pe-2"></i>Cash Incomes
+                      </h5>
+                      <p>Add Your Cash Incomes</p>
+                    </div>
+                    <div className="card m-2">
+                      <div className="p-4">
+                        <h1 className="ms-3 mb-4 display-6 align-items-center">
+                          <i className="bi bi-cash-coin me-4"></i>
+                          Add Your Cash Incomes
+                        </h1>
+                        <form onSubmit={onSubmit}>
+                          <CashIncome
+                            setSource={setSource}
+                            setDate={setDate}
+                            setAmount={setAmount}
+                          />
+                          <div className="align-items-center justify-content-center d-flex">
+                            <button
+                              className="btn save-btn"
+                              /* TODO: implement logic instead of CONST value */
+                              type="submit"
+                              disabled={
+                                amount <= 0 ||
+                                date.trim() === "" ||
+                                source.trim() === ""
+                              }
+                              id="saveBtn"
+                            >
+                              Save
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
                   </div>
-                </form>
+                </div>
               </div>
             </div>
-          </div>
+          </section>
         </div>
       </div>
     </>
